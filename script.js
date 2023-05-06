c980aafb3e56b25a167c3b5ceea5cf63961e1c8a
gopichand909 <100563483+gopichand909@users.noreply.github.com>
2023-05-06 19:17:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Get the navbar element
+const navbar = document.querySelector('.navbar');
+
+// Get the header height
+const headerHeight = document.querySelector('header').offsetHeight;
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', () => {
+  // Check if the user has scrolled past the header
+  if (window.scrollY > headerHeight) {
+    // Add the sticky class to the navbar
+    navbar.classList.add('sticky');
+  } else {
+    // Remove the sticky class from the navbar
+    navbar.classList.remove('sticky');
+  }
+});
